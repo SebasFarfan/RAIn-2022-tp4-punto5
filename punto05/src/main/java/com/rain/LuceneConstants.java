de53a96351581884bd5ebd2e3ede6d2bfb56e1d5
Sebas <sebas.654@gmail.com>
2022-05-24 01:13:29 -0300

    Agrego clase LuceneConstants.java

---
+++ punto05/src/main/java/com/rain/LuceneConstants.java
@@ -0,0 +1,20 @@
+package com.rain;
+
+public class LuceneConstants {
+    public static final String CONTENTS = "contents";
+    public static final String FILE_NAME = "filename";
+    public static final String FILE_PATH = "filepath";
+    public static final int MAX_SEARCH = 10;
+
+    // constantes para color de letras en consola
+    public static final String BLACK = "\033[30m";
+    public static final String RED = "\033[31m";
+    public static final String GREEN = "\033[32m";
+    public static final String YELLOW = "\033[33m";
+    public static final String BLUE = "\033[34m";
+    public static final String PURPLE = "\033[35m";
+    public static final String CYAN = "\033[36m";
+    public static final String WHITE = "\033[37m";
+    public static final String BG_BLUE = "\033[44m";
+    public static final String RESET = "\u001B[0m";
+}
